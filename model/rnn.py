c0f956c10c490c2e6cc113ac035449142ea01d77
Daniel Hershcovich <daniel.hershcovich@gmail.com>
2019-01-07 10:34:00 +0200

    Fix IndexError: invalid index of a 0-dim tensor. Use tensor.item() to convert a 0-dim tensor to a Python number

--- model/rnn.py
+++ model/rnn.py
@@ -43,7 +43,7 @@ class RelationModel(object):
         loss.backward()
         torch.nn.utils.clip_grad_norm(self.model.parameters(), self.opt['max_grad_norm'])
         self.optimizer.step()
-        loss_val = loss.data[0]
+        loss_val = loss.data.item()
         return loss_val
 
     def predict(self, batch, unsort=True):
